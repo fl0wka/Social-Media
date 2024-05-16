a21e8bee6bea04e8872e18d56a6bc10a5f8107f1
Юнусов Вадим <101979218+fl0wka@users.noreply.github.com>
2024-05-16 23:05:18 +0200

    Added Post Share component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import ProfileSide from "../../components/profileSide/ProfileSide"
+import PostSide from "../../components/PostSide/PostSide"
 import "./Home.css"
 
 const Home = () => {
 	return (
 		<div className="Home">
 			<ProfileSide />
-			<div className="postSide">Post</div>
+			<PostSide />
 			<div className="rightSide">Right Side</div>
 		</div>
 	)
