c2c2fa2c8d6d30da47ebdaf286f9010b3dc9b995
Юнусов Вадим <101979218+fl0wka@users.noreply.github.com>
2024-05-17 23:10:22 +0200

    Added Posts and RightSide components

---
+++ client/src/Data/PostsData.js
@@ -0,0 +1,27 @@
+import postPic1 from "../img/postpic1.jpg"
+import postPic2 from "../img/postpic2.jpg"
+import postPic3 from "../img/postpic3.JPG"
+
+export const PostsData = [
+	{
+		img: postPic1,
+		name: "Tasi",
+		desc: "Hey, guys!",
+		likes: 1999,
+		liked: true,
+	},
+	{
+		img: postPic2,
+		name: "Mary",
+		desc: "I am beautiful",
+		likes: 9999,
+		liked: false,
+	},
+	{
+		img: postPic3,
+		name: "Ally",
+		desc: "Go McDonalds",
+		likes: 672,
+		liked: false,
+	},
+]
---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData = [
+	{
+		name: "Minions",
+		shares: 97,
+	},
+	{
+		name: "Avanhers",
+		shares: 80.5,
+	},
+	{
+		name: "Jakekeepcode",
+		shares: 75.5,
+	},
+	{
+		name: "ReactJS",
+		shares: 72,
+	},
+	{
+		name: "J.N.Hope",
+		shares: 71.9,
+	},
+	{
+		name: "Car place",
+		shares: 20,
+	},
+]
