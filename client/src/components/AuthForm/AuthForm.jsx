e4ac3797f61a1881880e2dda49df14377d84c652
Юнусов Вадим <101979218+fl0wka@users.noreply.github.com>
2024-05-20 00:51:39 +0200

    adding logic to the authorization page and some code refactoring

---
+++ client/src/components/AuthForm/AuthForm.jsx
@@ -0,0 +1,106 @@
+import React from "react"
+import "./AuthForm.css"
+
+const AuthForm = ({
+	data,
+	isSignUp,
+	isConfirmPass,
+	setIsSignUp,
+	onChange,
+	onSubmit,
+	resetForm,
+	firstName,
+	lastName,
+	userName,
+	password,
+	confirmPass,
+}) => {
+	return (
+		<div className="a-right">
+			<form className="infoForm authForm" onSubmit={onSubmit}>
+				<h3>{isSignUp ? "Sign up" : "Log In"}</h3>
+
+				{isSignUp && (
+					<div>
+						<input
+							type="text"
+							placeholder="First Name"
+							className="infoInput"
+							name={firstName}
+							onChange={onChange}
+							value={data[firstName]}
+						/>
+						<input
+							type="text"
+							placeholder="Last Name"
+							className="infoInput"
+							name={lastName}
+							onChange={onChange}
+							value={data[lastName]}
+						/>
+					</div>
+				)}
+
+				<div>
+					<input
+						type="text"
+						placeholder="Username"
+						className="infoInput"
+						name={userName}
+						onChange={onChange}
+						value={data[userName]}
+					/>
+				</div>
+
+				<div>
+					<input
+						type="password"
+						placeholder="Password"
+						className="infoInput"
+						name={password}
+						onChange={onChange}
+						value={data[password]}
+					/>
+					{isSignUp && (
+						<input
+							type="password"
+							placeholder="Confirm Password"
+							className="infoInput"
+							name={confirmPass}
+							onChange={onChange}
+							value={data[confirmPass]}
+						/>
+					)}
+				</div>
+
+				{isSignUp && (
+					<span
+						className="confirmPassError"
+						style={{ display: isConfirmPass ? "none" : "block" }}
+					>
+						* Confirm Password is not same
+					</span>
+				)}
+
+				<div>
+					<span
+						style={{ fontSize: "12px", cursor: "pointer" }}
+						onClick={() => {
+							setIsSignUp(prev => !prev)
+							resetForm()
+						}}
+					>
+						{isSignUp
+							? "Already have an account. Login!"
+							: "Don't have an account. Sign up"}
+					</span>
+					<button className="button infoButton" type="sumbit">
+						{isSignUp ? "Sign up" : "Log In"}
+					</button>
+				</div>
+			</form>
+		</div>
+	)
+}
+
+export default AuthForm
--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,15 +1,58 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Auth.css"
 import AuthWebName from "../../components/AuthWebName/AuthWebName"
-import SignIn from "../../components/SignIn/SignIn"
-import SignUp from "../../components/SignUp/SignUp"
+import AuthForm from "../../components/AuthForm/AuthForm"
 
 const Auth = () => {
+	const initialState = {
+		firstname: "",
+		lastname: "",
+		username: "",
+		password: "",
+		confirmpass: "",
+	}
+	const [isSignUp, setIsSignUp] = useState(false)
+	const [data, setData] = useState(initialState)
+
+	const handleChange = e => {
+		setData({ ...data, [e.target.name]: e.target.value })
+	}
+	const [isConfirmPass, setIsConfirmPass] = useState(true)
+	const handleSubmit = e => {
+		e.preventDefault()
+
+		// Делаем проверку совпадения введенных значений полей "password" и "confirmpass"
+		if (isSignUp) {
+			if (data.password !== data.confirmpass) {
+				setIsConfirmPass(false)
+			}
+		}
+	}
+	const resetForm = () => {
+		setIsConfirmPass(true)
+		setData(initialState)
+	}
+
 	return (
 		<div className="Auth">
+			{/* Left Side */}
 			<AuthWebName />
-			{/* <SignUp /> */}
-			<SignIn />
+
+			{/* Right Side */}
+			<AuthForm
+				data={data}
+				isSignUp={isSignUp}
+				isConfirmPass={isConfirmPass}
+				setIsSignUp={setIsSignUp}
+				onChange={handleChange}
+				onSubmit={handleSubmit}
+				resetForm={resetForm}
+				firstName="firstname"
+				lastName="lastname"
+				userName="username"
+				password="password"
+				confirmPass="confirmpass"
+			/>
 		</div>
 	)
 }
