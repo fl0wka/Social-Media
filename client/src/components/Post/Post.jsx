e4ca03cdcfea69424c26ac3ea15d565157ee8f16
Юнусов Вадим <101979218+fl0wka@users.noreply.github.com>
2024-06-26 23:56:15 +0200

    Added comment button functionality

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import "./Post.css"
 import Comment from "../../img/comment.png"
 import Share from "../../img/share.png"
@@ -12,12 +12,36 @@ const Post = ({ data }) => {
 	const { user } = useSelector(getUser())
 	const [isLiked, setIsLiked] = useState(data.likes.includes(user._id))
 	const [likes, setLikes] = useState(data.likes.length)
+	const [isOpen, setIsOpen] = useState(false)
+	const [comment, setComment] = useState("")
+	const refInput = useRef()
 
 	const handleLike = () => {
 		setIsLiked(prev => !prev)
 		postService.likePost(data._id, user._id)
 		isLiked ? setLikes(prev => prev - 1) : setLikes(prev => prev + 1)
 	}
+
+	const handleComment = () => {
+		setIsOpen(prev => !prev)
+		setComment("")
+	}
+
+	const handleChange = e => {
+		setComment(e.target.value)
+	}
+
+	const handleSendComment = e => {
+		e.preventDefault()
+		console.log(comment)
+		setComment("")
+	}
+
+	useEffect(() => {
+		if (isOpen) {
+			refInput.current.focus()
+		}
+	}, [isOpen])
 
 	return (
 		<div className="Post">
@@ -33,13 +57,31 @@ const Post = ({ data }) => {
 					style={{ cursor: "pointer" }}
 					onClick={handleLike}
 				/>
-				<img src={Comment} alt="" />
-				<img src={Share} alt="" />
+				<img
+					onClick={handleComment}
+					style={{ width: "27px", height: "27px", cursor: "pointer" }}
+					src={Comment}
+					alt=""
+				/>
+				<img style={{ cursor: "pointer" }} src={Share} alt="" />
 			</div>
 
 			<span style={{ color: "var(--gray)", fontSize: "12px" }}>
 				{likes} likes
 			</span>
+
+			{isOpen && (
+				<form style={{ display: "flex", gap: "1rem" }} type="submit">
+					<input
+						ref={refInput}
+						type="text"
+						placeholder="Your comment"
+						onChange={handleChange}
+						value={comment}
+					/>
+					<button onClick={handleSendComment}>Send</button>
+				</form>
+			)}
 
 			<div className="details">
 				<span>
